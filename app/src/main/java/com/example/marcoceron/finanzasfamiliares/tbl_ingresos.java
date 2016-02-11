17538da939a42ae260dfff9ce0108a8c7f7657b4
markceron <markceron@gmail.com>
2016-02-11 15:50:41 -0600

    20160211.1

--- app/src/main/java/com/example/marcoceron/finanzasfamiliares/tbl_ingresos.java
+++ app/src/main/java/com/example/marcoceron/finanzasfamiliares/tbl_ingresos.java
@@ -6,7 +6,8 @@ package com.example.marcoceron.finanzasfamiliares;
 public class tbl_ingresos {
     public static final String TABLE_NAME = "tbl_ingresos";
 
-    public static final String CREATE_DB_TABLE = "CREATE TABLE " + TABLE_NAME + " (id INTEGER PRIMARY KEY ASC AUTOINCREMENT, idFondos INTEGER REFERENCES tbl_fondos (id) ON DELETE CASCADE ON UPDATE CASCADE, idCuenta INTEGER REFERENCES tbl_cuentas (id) ON DELETE CASCADE ON UPDATE CASCADE, fecha DATE NOT NULL, descripcion STRING (50) NOT NULL, monto DECIMAL (6, 2) NOT NULL);";
+    public static final String CREATE_DB_TABLE = "CREATE TABLE " + TABLE_NAME + " (id INTEGER PRIMARY KEY ASC AUTOINCREMENT, idFondos INTEGER REFERENCES tbl_fondos (id) ON DELETE CASCADE ON UPDATE CASCADE, " +
+            "idCuenta INTEGER REFERENCES tbl_cuentas (id) ON DELETE CASCADE ON UPDATE CASCADE, fecha DATE NOT NULL, descripcion STRING (50) NOT NULL, monto DECIMAL (6, 2) NOT NULL);";
 
-
+    public static final String SELECT_ALL = "Select * from " + TABLE_NAME;
 }
